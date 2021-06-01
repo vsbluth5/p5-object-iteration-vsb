2d5b9dc0d585c683eeeb6b29e371cce05a7ddfd1
Glitch (nostalgic-healthy-ceratonykus) <none>
2021-06-01 15:45:17 +0000

    Import of upperlinecode/object-iteration-mini-lab-javascript

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Get Started!
+# Object Iteration Practice
 
-Add files and start typing 🎉
+[![Run on Repl.it](https://repl.it/badge/github/upperlinecode/object-iteration-mini-lab-javascript)](https://repl.it/github/upperlinecode/object-iteration-mini-lab-javascript)
+
+### The goal
+
+In this lab, you'll start to deal with objects that organize data more and more clearly. We'll start with a list of favorite books, but each book is keyed to a person's name.
+
+For example, to print Zara's favorite book, you'd use the code `console.log(favoriteBooks["zara"])`.
+
+Open up iterate.js, code out solutions to the challenges, and run it to test your answers.
---
+++ iterate.js
@@ -0,0 +1,91 @@
+// Here's an object of favorite books, each keyed to a different person.
+let favoriteBooks = {
+  "diana": "Crime and Punishment",
+  "sophie": "The Secret History",
+  "cory": "Fahrenheit 451",
+  "gabe": "The Hobbit",
+  "danny": "Lonesome Dove",
+  "dan": "Don Quixote",
+  "katie": "10:04",
+  "zara": "Rebecca",
+  "david": "The Sun Also Rises",
+  "alexandra": "Hyperbole and a Half",
+  "martin": "The House on Mango Street",
+  "jeff": "The Hitchhiker's Guide to the Galaxy"
+}
+
+// 1. Print out Martin's favorite book.
+
+
+
+// 2. Print out Alexandra's favorite book.
+
+
+
+
+// 3. Before we get started, let's adjust a few things. First off, Jeff just read a new book called "The Martian" and loved it. With just one line of code, change Jeff's favorite book to "The Martian"
+
+
+
+// 4. Print out Jeff's favorite book to confirm that the change was successful.
+
+
+
+
+// 5. We should also add Ronald to the list. His favorite book is "Lies my History Teacher Told Me."
+//    Add that info to the object as a new key-value pair.
+
+
+
+// 6. Confirm that Ronald's book was added by printing it out.
+
+
+
+
+// LEVEL 2: In the addressBook.js file, there's an array called "contacts" with 100 objects inside of it. Each dictionary representes a person, and has that person's name, phone number, email, the company they work for, and their address.
+//    That means you could print the 7th person's email using this line of code:
+//    console.log(contacts[6]["email"]) // Uncomment this to see if it works. (Remember, list indexing starts at zero, so we use the number 6 to access the 7th name).
+
+// 8. Print the address book.
+
+
+
+
+// 9. Print out the fifth person from the the address book.
+
+
+
+
+// 10. Print out the email of the 10th person from the address book.
+
+
+
+// 11 Print out the name of the first person from the address book.
+
+
+
+// 12. Print out the name and phone number of the first person in the address book in the following format:
+//    "_____ can be reached at ______" filling in the blanks with the person's name and phone number.
+
+
+
+
+
+
+// 13. Print out the same information "_____ can be reached at ______" with names and phone numbers for all 100 of our contacts.
+
+
+
+
+
+
+// 14. We're going to send a mass text to all our contacts, so we need everyone's phone number. Our texting program can do it in an instant if we put all the numbers into an array.
+//    Create an array called phoneNumbers, iterate over our contacts, and add each person's number to that array. Print out the list when you're done to check your work.
+
+
+
+
+
+
+// 15. If we wanted to send an email that was only useful for college students and their professors, we could send that email ONLY to folks in our contacts whose email addresses end in ".edu".
+//    Create an array called eduEmails and put every .edu email address from our contacts into that array. Print the array when you're done to check your work.
